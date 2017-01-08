f1e4f8a79feb780a24cf328f2d488f861a5184dc
Tamas K. Papp <tkpapp@gmail.com>
2017-01-08 17:25:40 +0100

    Added Mamba conversions (preliminary).

---
+++ src/mamba.jl
@@ -0,0 +1,9 @@
+## conversion from the output of Mamba
+function PosteriorDraws(chain::Mamba.ModelChains)
+    all(findfirst(name, '[') == 0 for name in chain.names) ||
+        error("This function can't handle vector and matrix values (yet).")
+    keys = map(Symbol, chain.names)
+    vars = [vec(chain.value[:, var_index, :])
+            for var_index in indices(chain.value,2)]
+    PosteriorDraws(keys, vars)
+end
