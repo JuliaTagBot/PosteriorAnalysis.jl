d2d485b5e246f2fdd8fea551d3bcbeb4ad8b97ce
Tamas K. Papp <tkpapp@gmail.com>
2017-01-28 18:48:46 +0100

    Fixed Unicode bug.

--- src/mamba.jl
+++ src/mamba.jl
@@ -1,6 +1,6 @@
 ## conversion from the output of Mamba
 function PosteriorDraws(chain::Mamba.ModelChains)
-    all(findfirst(name, '[') == 0 for name in chain.names) ||
+    all(search(name, '[') == 0 for name in chain.names) ||
         error("This function can't handle vector and matrix values (yet).")
     keys = map(Symbol, chain.names)
     vars = [vec(chain.value[:, var_index, :])
